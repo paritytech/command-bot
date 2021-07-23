3007e65502e630185734a197aab22dc86912367b
joao-paulo-parity <joao-paulo@parity.io>
2021-07-23 06:14:46 -0300

    move non-probot env vars processing into the main function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createAppAuth } from "@octokit/auth-app"
 import { Octokit } from "@octokit/rest"
 import assert from "assert"
+import fs from "fs"
+import path from "path"
 import { Probot, run } from "probot"
 
 import { botMentionPrefix } from "src/constants"
@@ -11,21 +13,6 @@ import { Logger } from "./logger"
 import { AppState } from "./types"
 import { getPostPullRequestResult, getPullRequestHandleId } from "./utils"
 import { getWebhooksHandlers, setupEvent } from "./webhook"
-
-const allowedOrganizations = (process.env.ALLOWED_ORGANIZATIONS ?? "")
-  .split(",")
-  .filter(function (value) {
-    return value.length !== 0
-  })
-  .map(function (value) {
-    const parsedValue = parseInt(value)
-    assert(parsedValue)
-    return parsedValue
-  })
-assert(allowedOrganizations.length)
-
-assert(process.env.DB_PATH)
-const db = getDb(process.env.DB_PATH)
 
 assert(process.env.APP_ID)
 const appId = parseInt(process.env.APP_ID)
@@ -44,17 +31,6 @@ const clientId = process.env.CLIENT_ID
 
 assert(process.env.CLIENT_SECRET)
 const clientSecret = process.env.CLIENT_SECRET
-
-assert(process.env.ROCOCO_WEBSOCKET_ADDRESS)
-assert(process.env.WESTEND_WEBSOCKET_ADDRESS)
-assert(process.env.POLKADOT_WEBSOCKET_ADDRESS)
-assert(process.env.KUSAMA_WEBSOCKET_ADDRESS)
-const nodesAddresses = {
-  rococo: process.env.ROCOCO_WEBSOCKET_ADDRESS,
-  westend: process.env.WESTEND_WEBSOCKET_ADDRESS,
-  polkadot: process.env.POLKADOT_WEBSOCKET_ADDRESS,
-  kusama: process.env.KUSAMA_WEBSOCKET_ADDRESS,
-}
 
 const setupProbot = async function (state: AppState) {
   const { bot, logger } = state
@@ -98,6 +74,36 @@ const requeueUnterminated = async function ({
 
 const main = async function (bot: Probot) {
   const version = new Date().toISOString()
+
+  assert(process.env.ROCOCO_WEBSOCKET_ADDRESS)
+  assert(process.env.WESTEND_WEBSOCKET_ADDRESS)
+  assert(process.env.POLKADOT_WEBSOCKET_ADDRESS)
+  assert(process.env.KUSAMA_WEBSOCKET_ADDRESS)
+  const nodesAddresses = {
+    rococo: process.env.ROCOCO_WEBSOCKET_ADDRESS,
+    westend: process.env.WESTEND_WEBSOCKET_ADDRESS,
+    polkadot: process.env.POLKADOT_WEBSOCKET_ADDRESS,
+    kusama: process.env.KUSAMA_WEBSOCKET_ADDRESS,
+  }
+
+  const allowedOrganizations = (process.env.ALLOWED_ORGANIZATIONS ?? "")
+    .split(",")
+    .filter(function (value) {
+      return value.length !== 0
+    })
+    .map(function (value) {
+      const parsedValue = parseInt(value)
+      assert(parsedValue)
+      return parsedValue
+    })
+  assert(allowedOrganizations.length)
+
+  assert(process.env.DB_PATH)
+  const lockPath = path.join(process.env.DB_PATH, "LOCK")
+  if (fs.existsSync(lockPath)) {
+    fs.unlinkSync(lockPath)
+  }
+  const db = getDb(process.env.DB_PATH)
 
   const authInstallation = createAppAuth({
     appId,
