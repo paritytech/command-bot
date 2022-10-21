16c2edec86e4231eb70e173dd0bb80dca8943259
Mak <mordamax@users.noreply.github.com>
2022-10-21 14:30:00 +0300

    Update src/gitlab.spec.ts
    Signed-off-by: Mak <mordamax@users.noreply.github.com>

--- src/gitlab.spec.ts
+++ src/gitlab.spec.ts
@@ -4,7 +4,6 @@ import { Task } from "./task"
 
 logger.options.minLogLevel = "fatal"
 
-// eslint-disable-next-line no-empty-pattern
 function getTaskStub(opts: { vars: Record<string, string> } = { vars: {} }): Task {
   const task: Task = {
     gitlab: {
