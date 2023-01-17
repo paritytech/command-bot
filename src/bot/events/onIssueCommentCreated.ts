c5acc03acf1ece55d55aa53dda0f6bea8a7e42a1
Mak <mordamax@gmail.com>
2023-01-17 00:24:37 +0000

    swap handlers

--- src/bot/events/onIssueCommentCreated.ts
+++ src/bot/events/onIssueCommentCreated.ts
@@ -70,77 +70,6 @@ export const onIssueCommentCreated: WebhookHandler<"issue_comment.created"> = as
     for (const parsedCommand of commands) {
       logger.debug({ parsedCommand }, "Processing parsed command")
 
-      if (parsedCommand instanceof CancelCommand) {
-        const { taskId } = parsedCommand
-
-        const cancelledTasks: PullRequestTask[] = []
-        const failedToCancelTasks: PullRequestTask[] = []
-
-        for (const { task } of await getSortedTasks(ctx)) {
-          if (task.tag !== "PullRequestTask") {
-            continue
-          }
-
-          if (taskId) {
-            if (task.id !== taskId) {
-              continue
-            }
-          } else {
-            if (
-              task.gitRef.upstream.owner !== pr.owner ||
-              task.gitRef.upstream.repo !== pr.repo ||
-              task.gitRef.prNumber !== pr.number
-            ) {
-              continue
-            }
-          }
-
-          try {
-            await cancelTask(ctx, task)
-            cancelledTasks.push(task)
-          } catch (error) {
-            failedToCancelTasks.push(task)
-            logger.error(error, `Failed to cancel task ${task.id}`)
-          }
-        }
-
-        for (const cancelledTask of cancelledTasks) {
-          if (cancelledTask.comment.id === undefined) {
-            continue
-          }
-          try {
-            await updateComment(ctx, octokit, {
-              ...commentParams,
-              comment_id: cancelledTask.comment.id,
-              body: `@${requester} \`${cancelledTask.command}\`${
-                cancelledTask.gitlab.pipeline === null ? "" : ` (${cancelledTask.gitlab.pipeline.jobWebUrl})`
-              } was cancelled in ${comment.html_url}`,
-            })
-          } catch (error) {
-            logger.error(
-              { error, task: cancelledTask },
-              `Failed to update the cancel comment of task ${cancelledTask.id}`,
-            )
-          }
-        }
-
-        if (failedToCancelTasks.length) {
-          return getError(
-            `Successfully cancelled the following tasks: ${JSON.stringify(
-              cancelledTasks.map((task) => task.id),
-            )}\n\nFailed to cancel the following tasks: ${JSON.stringify(failedToCancelTasks.map((task) => task.id))}`,
-          )
-        }
-
-        if (cancelledTasks.length === 0) {
-          if (taskId) {
-            return getError(`No task matching ${taskId} was found`)
-          } else {
-            return getError("No task is being executed for this pull request")
-          }
-        }
-      }
-
       if (parsedCommand instanceof GenericCommand) {
         const installationId = installation?.id
         if (!installationId) {
@@ -215,6 +144,77 @@ export const onIssueCommentCreated: WebhookHandler<"issue_comment.created"> = as
           updateProgress,
         })
         await updateProgress(queueMessage)
+      }
+
+      if (parsedCommand instanceof CancelCommand) {
+        const { taskId } = parsedCommand
+
+        const cancelledTasks: PullRequestTask[] = []
+        const failedToCancelTasks: PullRequestTask[] = []
+
+        for (const { task } of await getSortedTasks(ctx)) {
+          if (task.tag !== "PullRequestTask") {
+            continue
+          }
+
+          if (taskId) {
+            if (task.id !== taskId) {
+              continue
+            }
+          } else {
+            if (
+              task.gitRef.upstream.owner !== pr.owner ||
+              task.gitRef.upstream.repo !== pr.repo ||
+              task.gitRef.prNumber !== pr.number
+            ) {
+              continue
+            }
+          }
+
+          try {
+            await cancelTask(ctx, task)
+            cancelledTasks.push(task)
+          } catch (error) {
+            failedToCancelTasks.push(task)
+            logger.error(error, `Failed to cancel task ${task.id}`)
+          }
+        }
+
+        for (const cancelledTask of cancelledTasks) {
+          if (cancelledTask.comment.id === undefined) {
+            continue
+          }
+          try {
+            await updateComment(ctx, octokit, {
+              ...commentParams,
+              comment_id: cancelledTask.comment.id,
+              body: `@${requester} \`${cancelledTask.command}\`${
+                cancelledTask.gitlab.pipeline === null ? "" : ` (${cancelledTask.gitlab.pipeline.jobWebUrl})`
+              } was cancelled in ${comment.html_url}`,
+            })
+          } catch (error) {
+            logger.error(
+              { error, task: cancelledTask },
+              `Failed to update the cancel comment of task ${cancelledTask.id}`,
+            )
+          }
+        }
+
+        if (failedToCancelTasks.length) {
+          return getError(
+            `Successfully cancelled the following tasks: ${JSON.stringify(
+              cancelledTasks.map((task) => task.id),
+            )}\n\nFailed to cancel the following tasks: ${JSON.stringify(failedToCancelTasks.map((task) => task.id))}`,
+          )
+        }
+
+        if (cancelledTasks.length === 0) {
+          if (taskId) {
+            return getError(`No task matching ${taskId} was found`)
+          } else {
+            return getError("No task is being executed for this pull request")
+          }
+        }
       }
     }
   } catch (rawError) {
